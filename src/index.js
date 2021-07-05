d67002271e7a1f17c1d040b29016c4dbbafb179c
mishaplx <olegorlov100oleg@gmail.com>
2021-07-05 21:12:00 +0300

    fix :add logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    return (Array.isArray(array) == false || array.length == 0) ? 0 :array.sort((a, b) => a - b)[0];
+  
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return (Array.isArray(array) == false || array.length == 0) ? 0 : array.sort((a, b) => a - b)[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (Array.isArray(array) == false || array.length == 0){
+    return 0
+  }
+  else {
+      let summa = 0;
+      for (let i = 0; i < array.length; i++) {
+          summa += array[i];
+      }
+      return (Array.isArray(array) == false || array.length == 0) ? 0 : summa / array.length;
+    }
+  
 
-exports.avg = function avg (array) {
-  return 0;
-}
+};
